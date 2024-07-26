19815ef37b7370e4fed5abcbe968a788888d66d8
Konstantin Đorđević <vomindoraan@gmail.com>
2024-07-26 11:21:22 +0200

    Add build, run, update scripts

---
+++ build.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+gcc LangtonsAnt/*.c -std=gnu18 -pedantic -O2 -DSERIAL_COLORS -lm -lncurses -o "${1:-LangtonsAnt/LangtonsAnt}"
+#gcc LangtonsAnt/*.c -std=c18 -pedantic -O2 -DSERIAL_COLORS -lm $(xcurses-config --libs-static --cflags) -o "${1:-LangtonsAnt/LangtonsAnt}"
---
+++ update.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+git checkout suluv && git fetch && git reset --hard origin/suluv
+./build.sh
