5da1435568b72f4c29e0dbca75dfaf4a4230a293
Konstantin Đorđević <vomindoraan@gmail.com>
2024-09-01 13:26:38 +0200

    Stub ./run.sh for MacOS

--- run.sh
+++ run.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 cd LangtonsAnt
-lxterminal --geometry=1024x768 -t KIOSK_APP -e ./LangtonsAnt
+
+if command -v lxterminal &>/dev/null; then
+    lxterminal --geometry=1024x768 -t KIOSK_APP -e ./LangtonsAnt
+elif command -v osascript &>/dev/null; then
+    osascript -e 'tell app "Terminal" to do script "./LangtonsAnt"'
+    # TODO: Set geometry, position, font name and size
+fi
 
 # ~/.config/openbox/lxde-pi-rc.xml
 # <?xml version="1.0" encoding="UTF-8"?>
