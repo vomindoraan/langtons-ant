605e15f11447427bbb569d1ac3482b33f73d3927
Konstantin Đorđević <vomindoraan@gmail.com>
2025-11-01 13:37:45 +0100

    Increase optimization level

--- LangtonsAnt/scripts/build.sh
+++ LangtonsAnt/scripts/build.sh
@@ -3,9 +3,9 @@ SRC_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")/.." &>/dev/null && pwd)"
 
 FEATURES="-DSERIAL_COLORS_ON"  # SERIAL_COLORS_ON, GALLERY_MODE_ON
 
-C_FLAGS="-std=gnu18 -Wpedantic -Wall -Wextra -O2"
+C_FLAGS="-std=gnu18 -Wpedantic -Wall -Wextra -O3"
 LIB_CURSES="-lncursesw"
 #LIB_CURSES="$(xcurses-config --libs-static --cflags)"
-L_FLAGS="-lm $LIB_CURSES"
+L_FLAGS="-lm $LIB_CURSES -flto"
 
 gcc "$SRC_DIR"/*.c $FEATURES $C_FLAGS $L_FLAGS -o "${1:-LangtonsAnt}"
